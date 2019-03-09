ffa5871b2d2d4064ef81b381b18ef792317837ca
Manuel Lang <manuellang183@gmail.com>
2019-03-09 23:25:53 +0100

    added omniglot feature extraction

--- omniglot_mnist_features.py
+++ omniglot_mnist_features.py
@@ -5,6 +5,7 @@ Gets to 99.25% test accuracy after 12 epochs
 '''
 
 from __future__ import print_function
+
 import keras
 from keras.datasets import mnist
 from keras.models import Model, Sequential
@@ -12,9 +13,15 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-import numpy as np 
+from keras.utils import plot_model
 
+import numpy as np 
+import os
 import pandas as pd 
+from scipy.misc import imshow
+from sklearn import preprocessing
+from skimage.transform import resize
+from skimage.util import invert
 
 input_file = '../CMU/AlphaLinkage/AlphaLinkage/data/mnist/mnist_train.csv'
 df = pd.read_csv(input_file, header=None)
@@ -37,6 +44,7 @@ else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
+print(type(x_train))
 x_train = x_train.astype('float32')
 
 print('x_train shape:', x_train.shape)
@@ -67,10 +75,28 @@ model.fit(x_train, y_train,
           verbose=1)
 
 model_extract = Model(model.inputs, model.layers[-3].output) # Dense(128,...)
-cnn_features = model_extract.predict(x_train)
 
-with open('mnist_cnn.csv', 'a') as outf:
-    for label, feature_vector in zip(y, cnn_features):
-        data = np.insert(feature_vector, 0, label)
-        out = ','.join([str(num) for num in data])
-        outf.write(out + '\n')
+folder = "../CMU/AlphaLinkage/AlphaLinkage/data/omniglot/images_background/"
+
+for f in os.listdir(folder):
+    with open(os.path.join(folder, f)) as f_in:
+        df = pd.read_csv(f_in, header=None)
+        y = df.iloc[:,0]
+        image_data_raw = df.iloc[:,1:]
+        image_data_raw.astype('float32')
+        image_data = np.asarray([np.reshape(x, (105,105)) for x in image_data_raw.values])
+        image_data = np.asarray([[[pixel / 255 for pixel in row] for row in img] for img in image_data])
+        image_data = np.asarray([resize(img, (28, 28), anti_aliasing=True) for img in image_data])
+        image_data = np.asarray([invert(img) for img in image_data])
+        image_data = np.asarray([[[pixel * 255 for pixel in row] for row in img] for img in image_data])
+        if K.image_data_format() == 'channels_first':
+            image_data = image_data.reshape(image_data.shape[0], 1, img_rows, img_cols)
+        else:
+            image_data = image_data.reshape(image_data.shape[0], img_rows, img_cols, 1)
+        image_data = image_data.astype('float32')
+        cnn_features = model_extract.predict(image_data)
+        with open(os.path.join("../CMU/AlphaLinkage/AlphaLinkage/data/omniglot/images_background_cnn_features/", f), 'w+') as f_out:
+            for label, feature_vector in zip(y, cnn_features):
+                data = np.insert(feature_vector, 0, int(label))
+                out = ','.join([str(num) for num in data])
+                f_out.write(out + '\n')
